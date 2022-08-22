b1d04020a49ed7026449961e0d0c5ed32f995c4b
demaiodev <fml1041@gmail.com>
2022-08-22 05:01:37 +0000

    little bash scrip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build # build to dist
-cp -r ~/repos/astro-folio/dist/ ~/var/www # copy to nginx's path
+cp -r ~/repos/astro-folio/dist/ /var/www # copy to nginx's path
 nginx -s reload # reload nginx
